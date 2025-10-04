8ae4b37e1a0fe65ce7ae38a120b50cac5f6f9f0d
jeremy <jeremy.j.holloway@gmail.com>
2025-10-04 13:40:55 -0400

    add in final validation checks

--- app/main.py
+++ app/main.py
@@ -1,22 +1,27 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, Response
 from pathlib import Path
 from flask_json_schema import JsonSchema, JsonValidationError
 import vendorTranslation, erpTranslation, json
 
 
-erpSchemaPath = Path("/home/jeremy/Documents/invoice-translation/app/schemas/invoice-b-schema.json") 
+erpSchemaPath = Path("app/schemas/invoice-b-schema.json")
+vendorSchemaPath = Path("app/schemas/invoice-a-schema.json")
 
 with open(erpSchemaPath, 'r') as f:
     erpSchema = json.load(f)
+
+with open(vendorSchemaPath, 'r') as f:
+    vendorSchema = json.load(f)
 
 app = Flask(__name__)
 schema = JsonSchema(app)
 
 @app.errorhandler(JsonValidationError)
 def validation_error(e):
-    return jsonify({ 'error': e.message, 'errors': [validation_error.message for validation_error  in e.errors]})
+    return jsonify({ 'error': e.message, 'errors': [validation_error.message for validation_error  in e.errors]}), 400
 
 @app.route('/vendorInvoice', methods=['POST'])
+@schema.validate(vendorSchema)
 def vendorResponse():
     return vendorTranslation.vendorInvoiceTranslation(request.json)
 
