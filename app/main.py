1a527b5e00f47fcc2b38dc4a5a13342d6d66127c
jeremy <jeremy.j.holloway@gmail.com>
2025-10-04 13:13:03 -0400

    erp schema validation in place

--- app/main.py
+++ app/main.py
@@ -1,18 +1,20 @@
 from flask import Flask, request, jsonify
 from pathlib import Path
-from flask_json_schema import JsonSchema
+from flask_json_schema import JsonSchema, JsonValidationError
 import vendorTranslation, erpTranslation, json
 
 
-# Define the path to your JSON schema file
-erpSchemaPath = Path("/schemas/invoice-b-schema.json") 
+erpSchemaPath = Path("/home/jeremy/Documents/invoice-translation/app/schemas/invoice-b-schema.json") 
 
-# Open and load the JSON schema
 with open(erpSchemaPath, 'r') as f:
     erpSchema = json.load(f)
 
 app = Flask(__name__)
 schema = JsonSchema(app)
+
+@app.errorhandler(JsonValidationError)
+def validation_error(e):
+    return jsonify({ 'error': e.message, 'errors': [validation_error.message for validation_error  in e.errors]})
 
 @app.route('/vendorInvoice', methods=['POST'])
 def vendorResponse():
